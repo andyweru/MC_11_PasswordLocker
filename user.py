cc88efe16c3682673c1e1b53aa12bb31341e248f
Andrew Waweru <andyweru@gmail.com>
2018-08-24 16:21:52 +0300

    User class and Users Array add_user method created

---
+++ user.py
@@ -0,0 +1,26 @@
+class User:
+    """
+    Class Generates new instances of Users
+    """
+
+    Users = [] #list of users
+
+    username = ''
+    email = ''
+    password = ''
+
+    def __init__(self, username, email, password):
+        self.username = username
+        self.email = email
+        self.password = password
+
+
+    def add_user(self):
+
+        """
+        save_contact method saves contact objects into contact_list
+        """
+
+        User.Users.append(self)
+    
+
