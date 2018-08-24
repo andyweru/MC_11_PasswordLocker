d1055587ab26820ebb2eac8879fde00660882de3
Andrew Waweru <andyweru@gmail.com>
2018-08-24 16:27:35 +0300

    program test file created

---
+++ program_test.py
@@ -0,0 +1,34 @@
+import unittest
+from user import User
+from credential import Credential
+
+class TestProgram(unittest.TestCase):
+    """
+    Test class for defining test cases for program behaviors
+
+    """
+
+    def setUp(self):
+        """
+        Runs before each test case
+        """
+        self.new_user = User("YoungWeshy",  "andyweru@gmail.com", "password123")
+
+    def tearDown(self):
+        """
+        Runs after every test case
+        """
+        User.Users = []
+
+    def test_init(self):
+        """
+        Test whether object is initialized properly
+        """
+
+        self.assertEqual(self.new_user.username,"YoungWeshy")
+        self.assertEqual(self.new_user.email,"andyweru@gmail.com")
+        self.assertEqual(self.new_user.password,"password123")
+
+if __name__ == '__main__':
+    unittest.main()
+        
