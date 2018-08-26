5113d676cb14b3554ccad12e9ae3a625bf4cabc3
Andrew Waweru <andyweru@gmail.com>
2018-08-27 00:32:15 +0300

    added credential class properties

--- credential.py
+++ credential.py
@@ -1,7 +1,19 @@
 class Credential:
-    """
-    Class Generates new instances of Users
-    """
+    """ Class Generates new instances of account credentials """
 
+    Accounts = []
+
+    platform = ''
+    platform_username = ''
+    platform_password = ''
+
+    def __init__(self, platform, username, password):
+        self.platform = platform
+        self.platform_username = username
+        self.platform_password = password
     
 
+    def add_credentials(self):
+        """ save new credentials to Accounts List """
+
+        Credential.Accounts.append(self)
